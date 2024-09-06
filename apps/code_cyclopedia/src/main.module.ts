d81a3adbcf962ec52385b08eb3926257cf158e95
jooaogbriel <joao.ggwp@outlook.com>
2024-09-06 17:44:42 -0300

    refact: removing auth
      remove user authentication through github

--- apps/code_cyclopedia/src/main.module.ts
+++ apps/code_cyclopedia/src/main.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PostgresModule } from './infrastructure/db/postgres/postgres.module';
-import { AuthModule } from './presentation/oauth/auth.module';
 import { CreateUserModule } from './presentation/user/create-user/create-user.module';
 
 @Module({
   imports: [
     PostgresModule,
     CreateUserModule,
-    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
