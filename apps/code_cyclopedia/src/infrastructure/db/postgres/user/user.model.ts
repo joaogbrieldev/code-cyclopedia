cdc1f5c893c9e8b15057582b4714a2e06756ab59
jooaogbriel <joao.ggwp@outlook.com>
2024-09-07 14:45:58 -0300

    feat: one to many relationship added
      relationship between user model and documentation model

--- apps/code_cyclopedia/src/infrastructure/db/postgres/user/user.model.ts
+++ apps/code_cyclopedia/src/infrastructure/db/postgres/user/user.model.ts
@@ -2,7 +2,8 @@ import { IDocumentation } from '@code_cyclopedia/domain/contracts/entities/docum
 import { IRepository } from '@code_cyclopedia/domain/contracts/entities/repository';
 import { IUser } from '@code_cyclopedia/domain/contracts/entities/user';
 import { BaseModel } from 'libs/shared/infrastructure/db/postgres/models/base.model';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
+import { DocumentationModel } from '../documentation/documentation.model';
 
 @Entity({ name: 'users' })
 export class UserModel extends BaseModel implements IUser {
@@ -17,5 +18,11 @@ export class UserModel extends BaseModel implements IUser {
   @Column({ type: 'varchar', default: 'x' })
   password: string;
 
-  documentations: Array<IDocumentation>;
+  @OneToMany(
+    () => DocumentationModel,
+    (documentationModel: DocumentationModel) => documentationModel.ownerUser,
+    { cascade: true, eager: true },
+  )
+  @JoinColumn()
+  documentations: IDocumentation[];
 }
