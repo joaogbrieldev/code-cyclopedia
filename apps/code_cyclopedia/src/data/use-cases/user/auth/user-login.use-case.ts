4d355f6145114f786e45bbe4dae7f4e8543a3a8e
jooaogbriel <joao.ggwp@outlook.com>
2024-09-29 23:21:55 -0300

    feat: initial use case login
     create use case login with validations and bcrypt compare

--- apps/code_cyclopedia/src/data/use-cases/user/auth/user-login.use-case.ts
+++ apps/code_cyclopedia/src/data/use-cases/user/auth/user-login.use-case.ts
@@ -1,10 +1,27 @@
+import { IUserRepository } from '@code_cyclopedia/domain/contracts/repositories/user.repository';
 import {
   IUserLoginInput,
   IUserLoginUseCase,
 } from '@code_cyclopedia/domain/contracts/use-cases/auth/user-login';
+import * as bcrypt from 'bcrypt';
 
 export class UserLoginUseCase implements IUserLoginUseCase {
+  constructor(private _userRepository: IUserRepository) {}
   async execute(input: IUserLoginInput): Promise<void> {
-    console.log(input);
+    this._validateEmail(input.email);
+
+    const user = await this._userRepository.getOne(input.email);
+
+    await this._validatePassword(input.password, user.password);
+  }
+
+  private _validateEmail(email: string) {
+    if (email) throw new Error('Email is not provide');
+  }
+
+  private _validatePassword(password: string, userPassword: string) {
+    if (password) throw new Error('Password is not provide');
+    const isValid = bcrypt.compare(password, userPassword);
+    if (!isValid) throw new Error('Password is not valid');
   }
 }
